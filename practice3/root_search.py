c5c40bb2797055e8420f21a1998776561e4b8a7d
Ekmid <brotherekmid@gmail.com>
2023-09-08 01:33:41 +0300

    вап

---
+++ practice3/root_search.py
@@ -0,0 +1,9 @@
+def guess(root):
+    for number in range(1, root):
+        if root / number == number:
+            print(root / number, 'Попался, гадёныш!')
+            break
+        else:
+            print(f'{number} Да где он ёмае?')
+
+guess(9821956)
