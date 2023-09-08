d59e98a33c897f4b387a76a69cc0121caf05b596
Ekmid <brotherekmid@gmail.com>
2023-09-08 12:35:24 +0300

    NoD commit

--- practice3/simple_guess.py
+++ practice3/simple_guess.py
@@ -9,15 +9,12 @@
 
 # guess(5)
 
-root = 0
-
 def guess(root):
-    numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-    for number in numbers:
+    for number in range(1, root):
         if root / number == number:
             print(root / number)
             break
         else:
-            print(f'{number} НЕ ПОДОШЛО')
+            print(f'{number} СЛОЖНА СЛОЖНА')
 
-guess(9)
+guess(9821956)
