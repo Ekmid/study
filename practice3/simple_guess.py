f62704aa5f1a5a97ec674e556ab222464cd4e724
Ekmid <brotherekmid@gmail.com>
2023-09-09 12:02:07 +0300

    fixed

--- practice3/simple_guess.py
+++ practice3/simple_guess.py
@@ -10,11 +10,11 @@
 # guess(5)
 
 def guess(root):
-    for number in range(1, root):
+    for number in range(1, root+1):
         if root / number == number:
             print(root / number, 'Вот он с*ка!')
             break
         else:
             print(f'{number} СЛОЖНА СЛОЖНА')
 
-guess(9821956)
+guess(25)
--- race.py
+++ race.py
@@ -6,24 +6,25 @@ def print_race_results(results):
     winner = min(results.values(), key=lambda x: x['FinishedTimeSeconds'])
 
     # Выводим сообщение о победителе
-    winner_message = f"Выиграл - {winner['RacerName']}!!! Поздравляем!!"
-    print(winner_message)
+    winner_message = f"Выиграл - {winner['RacerName'].upper()}!!! Поздравляем!!"
+    print(winner_message + '\n')
     print("_" * len(winner_message))
+    print("\n")
     
     # Сортируем результаты гонки по времени
     sorted_results = sorted(results.values(), key=lambda x: x['FinishedTimeSeconds'])
 
     # Выводим первые три места
-    print("\nПервые три места:\n")
+    print("\nПервые три места:\n\n\n")
     for i, racer in enumerate(sorted_results[:3], start=1):
         print(f"Гонщик на {i} месте:\n")
-        print(f"Имя: {racer['RacerName']}\n")
-        print(f"Команда: {racer['RacerTeam']}\n")
+        print(f"   Имя: {racer['RacerName']}\n")
+        print(f"   Команда: {racer['RacerTeam']}\n")
         time_seconds = racer['FinishedTimeSeconds']
         hours, remainder = divmod(time_seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
         time_str = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
-        print(f"Время: {time_str} (H:M:S)\n")
+        print(f"   Время: {time_str} (H:M:S)\n")
         print("\n")
 
 if __name__ == "__main__":
